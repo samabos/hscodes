d5bfb667188a6297737d700a6b17b89dcca5b0c6
samabos <samabos@users.noreply.github.com>
2024-11-23 19:59:37 +0000

    change to Import

--- code.ui/components/hero.tsx
+++ code.ui/components/hero.tsx
@@ -13,7 +13,7 @@ export default function Header() {
     
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 items-center">
   <div className="text-center">
-    <Link href="/import">
+    <Link href="/Import">
       <img 
         src="/images/classification.svg" 
         className="mx-auto w-40 sm:w-60 md:w-72 xl:w-80 2xl:w-[40vw] transition-transform duration-300 ease-in-out hover:scale-105" 
